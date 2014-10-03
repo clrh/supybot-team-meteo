65eec1e2188d8e84c262142b68e4037e50088b1f
Claire Hernandez <claire.hernandez@biblibre.com>
2014-10-03 16:41:16 +0200

    just a supybot-plugin-create

---
+++ plugin.py
@@ -0,0 +1,24 @@
+###
+# Copyright (c) 2014, clrh
+# All rights reserved.
+#
+#
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class TeamMeteo(callbacks.Plugin):
+    """Add the help for "@plugin help TeamMeteo" here
+    This should describe *how* to use this plugin."""
+    pass
+
+
+Class = TeamMeteo
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
---
+++ test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2014, clrh
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class TeamMeteoTestCase(PluginTestCase):
+    plugins = ('TeamMeteo',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
