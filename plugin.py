69796dd280211e69ec7643eaf51a4d302a551a67
Claire Hernandez <claire.hernandez@biblibre.com>
2014-12-09 13:08:03 +0100

    green

--- plugin.py
+++ plugin.py
@@ -11,13 +11,14 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import csv
-import pprint
+#import wget
 
 class TeamMeteo(callbacks.Plugin):
     """Add the help for "@plugin help TeamMeteo" here
     This should describe *how* to use this plugin."""
     pass
 
+    metric_file = 'mock2.csv'
     metrics = {}
 
     def __init__(self, irc):
@@ -31,15 +32,19 @@ class TeamMeteo(callbacks.Plugin):
         """
         irc.reply(str('coucou'))
 
+#    def _getnewfile(self):
+#        url = 'https://docs.google.com/spreadsheets/d/1B_0sZ74_jrFZo1GbyGmUQe2CJ5gklevunvt5EFxUA7g/export?format=csv&gid=1272107489&single=true'
+#        filename = wget.download(url)
+#        print filename
+
     def _loadmetrics(self):
-        with open ('mock2.csv', 'r') as csvfile:
+        with open (self.metric_file, 'r') as csvfile:
             reader = csv.reader(csvfile, delimiter=';')
             for row in reader:
                 self.metrics[row[0]] = row[1]
 
     def getnbhl(self,irc,msg,args):
-        self._loadmetrics
-        print "pouet"
+        self._loadmetrics()
         print self.metrics
         irc.reply(str(self.metrics['demandes HL']))
 
